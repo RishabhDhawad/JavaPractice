4bfb697659b70173d49f0334c0688e6350694306
Rishabh Dhawad <rishabhdhawad29@gmail.com>
2022-08-11 23:59:53 +0530

    TypeCasting added

---
+++ TypeCasting.java
@@ -0,0 +1,9 @@
+public class TypeCasting 
+{
+	public static void main (String args[])
+	{
+		byte a = 10;
+		a = (byte) (a+5);
+		System.out.println (a);
+	}
+}
---
+++ TypeCasting2.java
@@ -0,0 +1,9 @@
+class TypeCasting2
+{
+	public static void main (String args[])
+	{
+		byte a = 10 , b = 20 , c;
+		c = (byte)(a+b);
+		System.out.println(a + b);
+	}
+}
