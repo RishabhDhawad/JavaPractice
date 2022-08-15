34a06139a8ed12e413c880b26f2c5d6bbff0b92f
Rishabh Dhawad <rishabhdhawad29@gmail.com>
2022-08-15 23:38:02 +0530

    primitive and user input files added

---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Input {
+	public static void main (String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter some value:");
+		int rollno = input.nextInt();
+		System.out.println("Your roll no is: " + rollno);
+	}
+}
---
+++ Primitives.java
@@ -0,0 +1,10 @@
+class Primitives {
+	public static void main (String args[]) {
+		int rollno = 64 ;
+		char letter = 'r';
+		float marks = 98.67f;
+		double LargeDecimalNumbers = 1384946.135424;
+		long LargeInteger = 478254982326L;
+		boolean check = true;
+	}
+}
